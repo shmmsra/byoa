f129db3acbdf8b7564f39bb6aba1e5116033c0e3
Shivam Mishra <shm.msra@gmail.com>
2026-01-07 14:25:15 +0530

    Fix App icon on Windows binary

--- src/native/resource/win/resource-ids.h
+++ src/native/resource/win/resource-ids.h
@@ -7,6 +7,8 @@
 // Resource IDs for embedded web resources
 // AUTO-GENERATED - DO NOT EDIT
 
+#define IDI_APP_ICON 101
+
 #define IDR_WEB_INDEX_HTML 100
 
 #define IDR_WEB_RESOURCE_BASE 1000
@@ -14,7 +16,7 @@
 #define IDR_WEB_INDEX_JS 1000
 
 // Resource ID to filename mapping
-inline const std::map<int, std::string>& getResourceMap() {
+inline const std::map<int, std::string> &getResourceMap() {
     static const std::map<int, std::string> resourceMap = {
         {IDR_WEB_INDEX_HTML, "index.html"},
         {IDR_WEB_INDEX_JS, "index.js"},
